9832bff3d14439f8de2a328662d6e20a0d2bbe6a
superssol <86730207+superssol@users.noreply.github.com>
2021-08-09 16:00:34 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+ <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2
+    }
+  },
+  created() {
+    console.log('created!', this.count)
+  },
+  mounted() {
+    console.log('mounted!')
+    console.log(document.querySelector('h1'))
+  }
+}
+</script>
